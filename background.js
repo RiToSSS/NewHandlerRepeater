9f9091828f6553177f00cfd71dc42c94ad7d1105
rita01-F <63978286+rita01-F@users.noreply.github.com>
2021-01-14 15:37:50 +1000

    Убран отладочный код, изменена фун-я чтения файла

--- background.js
+++ background.js
@@ -1,106 +1,99 @@
 let record = false;
 let repeat = false;
-let a;
-let aInput;
+let selectors;
+let text_input;
 let url;
-let i = 0;
-let j = 0;
+let index_selector = 0;
+let index_text = 0;
 let status = "Off";
 
 chrome.browserAction.setBadgeBackgroundColor({color: "red"});
 chrome.browserAction.setBadgeText({text: status});
+
+function downloadFile(el, text, name, type) {
+   let  file = new Blob([text], {type: type});
+   el.href = URL.createObjectURL(file);
+   el.download = name;
+}
 
 chrome.browserAction.onClicked.addListener(()=> {
    if (status == "Off") {
       chrome.browserAction.setBadgeBackgroundColor({color: "green"});
       status = "On";
       record = true;
-      a = [];
-      aInput = [];
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
          chrome.tabs.sendMessage(tabs[0].id, {type: 'record', subtype: 'start'}, function(message) {
-            url = message.data.url;
+            url = message.url;
          });
       });
    } else {
       chrome.browserAction.setBadgeBackgroundColor({color: "red"});
       status = "Off";
       record = false;
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-         chrome.tabs.sendMessage(tabs[0].id, {type: 'record', subtype: 'stop'});
-      });
+      // Единственный способ скачать файл - создать элемент <a> и симитировать его нажатие с помощью метода click(),
+      // так как вручную нажать на него через фоновую страницу невозможно
+      if (selectors.length > 0) {
+         $('body').append("<a id='download'></a>");
+         download = document.getElementById('download');
+         downloadFile(download, url + "\n" + JSON.stringify(selectors) + "\n" + JSON.stringify(text_input), 'repeat_script.json', 'text/plain')
+         download.click();
+         $('a#download').remove();
+      }
    }
+   selectors = [];
+   text_input = [];
    chrome.browserAction.setBadgeText({text: status});
 });
 
 chrome.runtime.onMessage.addListener(function(message, sender, response) {
    if (message.type == 'record') {
       if (message.subtype == 'getInfo') {
-         response ({data: {status: record}});
+         response ({status: record});
       }
 
       if (message.subtype == 'start') {
-         a = [];
-         aInput = [];
-         record = message.data.status;
-         url = message.data.url;
+         record = message.status;
+         url = message.url;
       }
 
       if (message.subtype == 'push') {
-         console.log("Нажат элемент: ", message.data.element);
-         a.push(message.data.element);
+         selectors.push(message.element);
       }
 
       if (message.subtype == 'pushInput') {
-         aInput.push(message.data.value);
-         console.log(JSON.stringify(aInput));
-      }
-
-      if (message.subtype == 'stop') {
-         record = message.data.status;
+         text_input.push(message.value);
       }
    }
 
    if (message.type == 'repeat') {
-      if (message.subtype == 'askRepeat') {
-         if ((a.length > 1) && (!repeat)) {
-            console.log("Переход на страницу: ", url);
+      if (message.subtype == 'setInfo') {
+         if (!repeat) {
+            selectors = JSON.parse(message.selectors);
+            text_input = JSON.parse(message.text_input);
             repeat = true;
-            console.log(JSON.stringify(a));
-            response ({data: {allow: repeat, href: url}});
-         } else {
-            console.log("Длина массива: ", a.length, "Идёт повтор: ", repeat);
-            response ({data: {allow: false}});
          }
-      }
-
-      if (message.subtype == 'start') {
-         response ({data: {allow: repeat, array: JSON.stringify(a)}});
+         response ({allow: repeat});
       }
 
       if (message.subtype == 'isGoing') {
          if (repeat) {
-            if (i < a.length) {
-               console.log("Нажимаем на элемент: ", a[i]);
-               if (a[i].indexOf("input") == 0) {
-                  console.log("Input... ", JSON.stringify(aInput[j]));
-                  response ({data: {allow: repeat, element: a[i], valueInput: aInput[j]}});
-                  j++;
+            if (index_selector < selectors.length) {
+               if (selectors[index_selector].indexOf("input") == 0) {
+                  response ({allow: repeat, element: selectors[index_selector], valueInput: text_input[index_text]});
+                  index_text++;
                } else {
-                  response ({data: {allow: repeat, element: a[i]}});
+                  response ({allow: repeat, element: selectors[index_selector]});
                }
-               i++;
+               index_selector++;
             } else {
-               i = 0;
+               index_selector = 0;
+               index_text = 0;
                repeat = false;
             }
          } else {
-            response ({data: {allow: false}});
+            repeat = false;
+            response ({allow: repeat});
          }
-      }
-
-      if (message.subtype == 'end') {
-         repeat = false;
       }
    }
 });
--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-let val;
-let a = [];
-let i = 0;
 let selector;
 
 function clicker() {
     chrome.runtime.sendMessage({type: 'repeat', subtype: 'isGoing'}, function(message) {
-        if (message.data.allow) {
-            let el = message.data.element;
+        if (message.allow) {
+            let el = message.element;
             let ind = 0;
             let selectors;
             let x = 0;
@@ -14,12 +11,10 @@ function clicker() {
                 x = el.substr(el.indexOf(" ") + 1);
                 el = el.substring(0, el.indexOf(" "));
             }
-            console.log("Нажимаем селектор: " + el + " с индексом: " + x);
             selectors = document.querySelectorAll(el);
             selectors[x].click();
             if (el.indexOf("input") == 0) {
-                console.log("Найден Input, заполняем...");
-                selectors[x].value = message.data.valueInput;
+                selectors[x].value = message.valueInput;
             }
         }
     });
@@ -28,26 +23,20 @@ function clicker() {
 chrome.runtime.onMessage.addListener(function(message, sender, response) {
     if (message.type == 'record') {
         if (message.subtype == "start") {
-            record = true;
-            response({data: {url: document.location.href}});
-        }
-        
-        if (message.subtype == "stop") {
-            record = false;
+            response({url: document.location.href});
         }
     }
 });
 
-$(document).ready(function(){ 
-    setInterval(clicker, 2000);
+$(document).ready(function() { 
+    timerClicker = setInterval(clicker, 3000);
 })
 
 
-addEventListener('mouseup', function (event) {
+addEventListener('mousedown', function (event) {
     chrome.runtime.sendMessage({type: 'record', subtype: 'getInfo'}, function(message) {
-        let status = message.data.status;
-        // Если предыдущий элемент - input, то берём его содержимое и отправляем в background.js
-        if (status) {
+        if (message.status) {
+            // Если предыдущий элемент - input, то берём его содержимое и отправляем в background.js
             if (selector) {
                 if (selector.indexOf("input") == 0) {
                     let el = selector;
@@ -58,9 +47,8 @@ addEventListener('mouseup', function (event) {
                         x = el.substr(el.indexOf(" ") + 1);
                         el = el.substring(0, el.indexOf(" "));
                     }
-                    console.log("Найден Input: " + el + " с индексом: " + x);
                     selectors = document.querySelectorAll(el);
-                    chrome.runtime.sendMessage({type:'record', subtype: 'pushInput', data: {value: selectors[x].value}});
+                    chrome.runtime.sendMessage({type:'record', subtype: 'pushInput', value: selectors[x].value});
                 }
             }
     
@@ -77,29 +65,48 @@ addEventListener('mouseup', function (event) {
                 idName = "";
             }
             selector = event.target.tagName.toLowerCase() + className + idName;
-            console.log("Нажат элемент: ", selector);
             if(document.querySelectorAll(selector).length > 1) {
                 selector += " " + $(event.target).index(selector);
             };
-            chrome.runtime.sendMessage({type:'record', subtype: 'push', data: {element: selector}});
+            chrome.runtime.sendMessage({type:'record', subtype: 'push', element: selector});
         }
     });
 }, true);
 
+function startRepeat(content) {
+    content = content.split("\n");
+    $('#uploadJSONfile').remove();
+    chrome.runtime.sendMessage({type: 'repeat', subtype: 'setInfo', selectors: content[1], text_input: content[2]}, function(message) {
+        if (message.allow) {
+            document.location.href = content[0];
+
+            // Необходимо обнулить интервал, иначе может начаться процесс нажатия на элементы ещё до загрузки стартовой страницы
+            clearInterval(timerClicker);
+        }
+    });
+}
+
+$(document).on('change', '#uploadJSONfile', function (e) {
+    if (window.FileList && window.File) {
+        const file = e.target.files[0];
+        const name = file.name ? file.name : 'NOT SUPPORTED';
+        if (name.indexOf(".json") != -1) {
+            const reader = new FileReader();
+            reader.addEventListener('load', event => {
+                let content = event.target.result;
+                startRepeat(content);
+            });
+            reader.readAsText(file);
+        }       
+    }
+});
+
 $(document).keyup(function (event) {
     if (event.key === "Escape" || event.keyCode === 27) {
         chrome.runtime.sendMessage({type: 'record', subtype: 'getInfo'}, function(message) {
-            let status = message.data.status;
-            if (!status) {
-                chrome.runtime.sendMessage({type: 'repeat', subtype: 'askRepeat'}, function(message) {
-                    if (message.data.allow) {
-                        document.location.href = message.data.href;
-                    } else {
-                        console.log("Вы не можете запустить функцию Repeat");
-                    }
-                });
-            } else {
-                console.log("Вы не можете запустить функцию Repeat, пока идёт запись");
+            // Загрузку из фоновой страницы нельзя симитировать через нажатие <input> (File chooser dialog can only be shown with a user activation)
+            if (!message.status) {
+                $('body').append("<input type='file' style='position: fixed; top: 0; left: 0;' id='uploadJSONfile'/>");
             }
         });
     }
