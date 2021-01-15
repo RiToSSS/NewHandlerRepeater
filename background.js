1654defec3356a58e989a714a4e11a8d548c80c4
Ritos <kutolvas@bk.ru>
2021-01-15 20:41:58 +1000

    Исправление стиля кода
    Исправлены глобальные переменные, в условном операторе заменена переменная status на record, создан один массив

--- background.js
+++ background.js
@@ -1,99 +1,98 @@
-let record = false;
-let repeat = false;
-let selectors;
-let text_input;
-let url;
-let index_selector = 0;
-let index_text = 0;
-let status = "Off";
+(function() {
 
-chrome.browserAction.setBadgeBackgroundColor({color: "red"});
-chrome.browserAction.setBadgeText({text: status});
+   let record = false;
+   let repeat = false;
+   let selectors;
+   let url;
+   let index_selector = 0;
+   let index_text = 0;
 
-function downloadFile(el, text, name, type) {
-   let  file = new Blob([text], {type: type});
-   el.href = URL.createObjectURL(file);
-   el.download = name;
-}
+   
+   chrome.browserAction.setBadgeBackgroundColor({color: "red"});
+   chrome.browserAction.setBadgeText({text: "Off"});
 
-chrome.browserAction.onClicked.addListener(()=> {
-   if (status == "Off") {
-      chrome.browserAction.setBadgeBackgroundColor({color: "green"});
-      status = "On";
-      record = true;
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-         chrome.tabs.sendMessage(tabs[0].id, {type: 'record', subtype: 'start'}, function(message) {
-            url = message.url;
+   function downloadFile(el, text, name, type) {
+      let  file = new Blob([text], {type: type});
+      el.href = URL.createObjectURL(file);
+      el.download = name;
+   }
+
+   chrome.browserAction.onClicked.addListener(()=> {
+      if (!record) {
+         chrome.browserAction.setBadgeBackgroundColor({color: "green"});
+         record = true;
+         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {type: 'record', subtype: 'start'}, function(message) {
+               url = message.url;
+            });
          });
-      });
-   } else {
-      chrome.browserAction.setBadgeBackgroundColor({color: "red"});
-      status = "Off";
-      record = false;
-      // Единственный способ скачать файл - создать элемент <a> и симитировать его нажатие с помощью метода click(),
-      // так как вручную нажать на него через фоновую страницу невозможно
-      if (selectors.length > 0) {
-         $('body').append("<a id='download'></a>");
-         download = document.getElementById('download');
-         downloadFile(download, url + "\n" + JSON.stringify(selectors) + "\n" + JSON.stringify(text_input), 'repeat_script.json', 'text/plain')
-         download.click();
-         $('a#download').remove();
-      }
-   }
-   selectors = [];
-   text_input = [];
-   chrome.browserAction.setBadgeText({text: status});
-});
-
-chrome.runtime.onMessage.addListener(function(message, sender, response) {
-   if (message.type == 'record') {
-      if (message.subtype == 'getInfo') {
-         response ({status: record});
-      }
-
-      if (message.subtype == 'start') {
-         record = message.status;
-         url = message.url;
-      }
-
-      if (message.subtype == 'push') {
-         selectors.push(message.element);
-      }
-
-      if (message.subtype == 'pushInput') {
-         text_input.push(message.value);
-      }
-   }
-
-   if (message.type == 'repeat') {
-      if (message.subtype == 'setInfo') {
-         if (!repeat) {
-            selectors = JSON.parse(message.selectors);
-            text_input = JSON.parse(message.text_input);
-            repeat = true;
+      } else {
+         chrome.browserAction.setBadgeBackgroundColor({color: "red"});
+         record = false;
+         // Единственный способ скачать файл - создать элемент <a> и симитировать его нажатие с помощью метода click(),
+         // так как вручную нажать на него через фоновую страницу невозможно
+         if (selectors.length > 0) {
+            $('body').append("<a id='download'></a>");
+            download = document.getElementById('download');
+            downloadFile(download, url + "\n" + JSON.stringify(selectors), 'repeat_script.json', 'text/plain')
+            download.click();
+            $('a#download').remove();
          }
-         response ({allow: repeat});
+      }
+      selectors = [[],[]];
+      record ? chrome.browserAction.setBadgeText({text: "On"}) : chrome.browserAction.setBadgeText({text: "Off"});
+   });
+
+   chrome.runtime.onMessage.addListener(function(message, sender, response) {
+      if (message.type == 'record') {
+         if (message.subtype == 'getInfo') {
+            response ({status: record});
+         }
+
+         if (message.subtype == 'start') {
+            record = message.status;
+            url = message.url;
+         }
+
+         if (message.subtype == 'push') {
+            selectors[0].push(message.element);
+         }
+
+         if (message.subtype == 'pushInput') {
+            selectors[1].push(message.value);
+         }
       }
 
-      if (message.subtype == 'isGoing') {
-         if (repeat) {
-            if (index_selector < selectors.length) {
-               if (selectors[index_selector].indexOf("input") == 0) {
-                  response ({allow: repeat, element: selectors[index_selector], valueInput: text_input[index_text]});
-                  index_text++;
-               } else {
-                  response ({allow: repeat, element: selectors[index_selector]});
-               }
-               index_selector++;
-            } else {
-               index_selector = 0;
-               index_text = 0;
-               repeat = false;
+      if (message.type == 'repeat') {
+         if (message.subtype == 'setInfo') {
+            if (!repeat) {
+               selectors = JSON.parse(message.selectors);
+               repeat = true;
             }
-         } else {
-            repeat = false;
             response ({allow: repeat});
          }
+
+         if (message.subtype == 'isGoing') {
+            if (repeat) {
+               if (index_selector < selectors[0].length) {
+                  if (selectors[0][index_selector].indexOf("input") == 0) {
+                     response ({allow: repeat, element: selectors[0][index_selector], valueInput: selectors[1][index_text]});
+                     index_text++;
+                  } else {
+                     response ({allow: repeat, element: selectors[0][index_selector]});
+                  }
+                  index_selector++;
+               } else {
+                  index_selector = 0;
+                  index_text = 0;
+                  repeat = false;
+               }
+            } else {
+               repeat = false;
+               response ({allow: repeat});
+            }
+         }
       }
-   }
-});
+   });
+   
+})();
--- script.js
+++ script.js
@@ -1,113 +1,107 @@
-let selector;
+(function() {
 
-function clicker() {
-    chrome.runtime.sendMessage({type: 'repeat', subtype: 'isGoing'}, function(message) {
-        if (message.allow) {
-            let el = message.element;
-            let ind = 0;
-            let selectors;
-            let x = 0;
-            if (el.indexOf(" ") != -1) {
-                x = el.substr(el.indexOf(" ") + 1);
-                el = el.substring(0, el.indexOf(" "));
-            }
-            selectors = document.querySelectorAll(el);
-            selectors[x].click();
-            if (el.indexOf("input") == 0) {
-                selectors[x].value = message.valueInput;
-            }
-        }
-    });
-}
+    let selector;
 
-chrome.runtime.onMessage.addListener(function(message, sender, response) {
-    if (message.type == 'record') {
-        if (message.subtype == "start") {
-            response({url: document.location.href});
-        }
-    }
-});
-
-$(document).ready(function() { 
-    timerClicker = setInterval(clicker, 3000);
-})
-
-
-addEventListener('mousedown', function (event) {
-    chrome.runtime.sendMessage({type: 'record', subtype: 'getInfo'}, function(message) {
-        if (message.status) {
-            // Если предыдущий элемент - input, то берём его содержимое и отправляем в background.js
-            if (selector) {
-                if (selector.indexOf("input") == 0) {
-                    let el = selector;
-                    let ind = 0;
-                    let selectors;
-                    let x = 0;
-                    if (el.indexOf(" ") != -1) {
-                        x = el.substr(el.indexOf(" ") + 1);
-                        el = el.substring(0, el.indexOf(" "));
-                    }
-                    selectors = document.querySelectorAll(el);
-                    chrome.runtime.sendMessage({type:'record', subtype: 'pushInput', value: selectors[x].value});
+    function clicker() {
+        chrome.runtime.sendMessage({type: 'repeat', subtype: 'isGoing'}, function(message) {
+            if (message.allow) {
+                let el = message.element;
+                let ind = 0;
+                let selectors;
+                let x = 0;
+                if (el.indexOf(" ") != -1) {
+                    x = el.substr(el.indexOf(" ") + 1);
+                    el = el.substring(0, el.indexOf(" "));
                 }
-            }
-    
-            let className = ".";
-            if(event.target.className) {
-                className += event.target.className.replace(new RegExp(" ", "g"), ".");
-            } else {
-                className = "";
-            }
-            let idName = "#";
-            if(event.target.id) {
-                idName += event.target.id;
-            } else {
-                idName = "";
-            }
-            selector = event.target.tagName.toLowerCase() + className + idName;
-            if(document.querySelectorAll(selector).length > 1) {
-                selector += " " + $(event.target).index(selector);
-            };
-            chrome.runtime.sendMessage({type:'record', subtype: 'push', element: selector});
-        }
-    });
-}, true);
-
-function startRepeat(content) {
-    content = content.split("\n");
-    $('#uploadJSONfile').remove();
-    chrome.runtime.sendMessage({type: 'repeat', subtype: 'setInfo', selectors: content[1], text_input: content[2]}, function(message) {
-        if (message.allow) {
-            document.location.href = content[0];
-
-            // Необходимо обнулить интервал, иначе может начаться процесс нажатия на элементы ещё до загрузки стартовой страницы
-            clearInterval(timerClicker);
-        }
-    });
-}
-
-$(document).on('change', '#uploadJSONfile', function (e) {
-    if (window.FileList && window.File) {
-        const file = e.target.files[0];
-        const name = file.name ? file.name : 'NOT SUPPORTED';
-        if (name.indexOf(".json") != -1) {
-            const reader = new FileReader();
-            reader.addEventListener('load', event => {
-                let content = event.target.result;
-                startRepeat(content);
-            });
-            reader.readAsText(file);
-        }       
-    }
-});
-
-$(document).keyup(function (event) {
-    if (event.key === "Escape" || event.keyCode === 27) {
-        chrome.runtime.sendMessage({type: 'record', subtype: 'getInfo'}, function(message) {
-            // Загрузку из фоновой страницы нельзя симитировать через нажатие <input> (File chooser dialog can only be shown with a user activation)
-            if (!message.status) {
-                $('body').append("<input type='file' style='position: fixed; top: 0; left: 0;' id='uploadJSONfile'/>");
+                selectors = document.querySelectorAll(el);
+                selectors[x].click();
+                if (el.indexOf("input") == 0) {
+                    selectors[x].value = message.valueInput;
+                }
             }
         });
     }
-});
+    
+    chrome.runtime.onMessage.addListener(function(message, sender, response) {
+        if (message.type == 'record') {
+            if (message.subtype == "start") {
+                response({url: document.location.href});
+            }
+        }
+    });
+    
+    $(document).ready(function() { 
+        timerClicker = setInterval(clicker, 3000);
+    })
+    
+    
+    addEventListener('mousedown', function (event) {
+        chrome.runtime.sendMessage({type: 'record', subtype: 'getInfo'}, function(message) {
+            if (message.status) {
+                // Если предыдущий элемент - input, то берём его содержимое и отправляем в background.js
+                if (selector) {
+                    if (selector.indexOf("input") == 0) {
+                        let el = selector;
+                        let ind = 0;
+                        let selectors;
+                        let x = 0;
+                        if (el.indexOf(" ") != -1) {
+                            x = el.substr(el.indexOf(" ") + 1);
+                            el = el.substring(0, el.indexOf(" "));
+                        }
+                        selectors = document.querySelectorAll(el);
+                        chrome.runtime.sendMessage({type:'record', subtype: 'pushInput', value: selectors[x].value});
+                    }
+                }
+        
+                let className = event.target.className ? "." + event.target.className.replace(new RegExp(" ", "g"), ".") : "";
+                let idName = event.target.id ? "#" + event.target.id : "";
+                selector = event.target.tagName.toLowerCase() + className + idName;
+                if(document.querySelectorAll(selector).length > 1) {
+                    selector += " " + $(event.target).index(selector);
+                };
+                chrome.runtime.sendMessage({type:'record', subtype: 'push', element: selector});
+            }
+        });
+    }, true);
+    
+    function startRepeat(content) {
+        content = content.split("\n");
+        $('#uploadJSONfile').remove();
+        chrome.runtime.sendMessage({type: 'repeat', subtype: 'setInfo', selectors: content[1]}, function(message) {
+            if (message.allow) {
+                document.location.href = content[0];
+    
+                // Необходимо обнулить интервал, иначе может начаться процесс нажатия на элементы ещё до загрузки стартовой страницы
+                clearInterval(timerClicker);
+            }
+        });
+    }
+    
+    $(document).on('change', '#uploadJSONfile', function (e) {
+        if (window.FileList && window.File) {
+            const file = e.target.files[0];
+            const name = file.name ? file.name : 'NOT SUPPORTED';
+            if (name.indexOf(".json") != -1) {
+                const reader = new FileReader();
+                reader.addEventListener('load', event => {
+                    let content = event.target.result;
+                    startRepeat(content);
+                });
+                reader.readAsText(file);
+            }       
+        }
+    });
+    
+    $(document).keyup(function (event) {
+        if (event.key === "Escape" || event.keyCode === 27) {
+            chrome.runtime.sendMessage({type: 'record', subtype: 'getInfo'}, function(message) {
+                // Загрузку из фоновой страницы нельзя симитировать через нажатие <input> (File chooser dialog can only be shown with a user activation)
+                if (!message.status) {
+                    $('body').append("<input type='file' style='position: fixed; top: 0; left: 0;' id='uploadJSONfile'/>");
+                }
+            });
+        }
+    });
+
+ })();
